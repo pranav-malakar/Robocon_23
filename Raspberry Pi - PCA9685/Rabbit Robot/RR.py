d085c556a6034b272ba6350c72060f365ac5e86a
Pranav Malakar <92641237+pranav-malakar@users.noreply.github.com>
2023-01-24 00:52:16 +0530

    Relay, Rotation, Drift Added

--- Raspberry Pi - PCA9685/Rabbit Robot/RR.py
+++ Raspberry Pi - PCA9685/Rabbit Robot/RR.py
@@ -9,7 +9,6 @@ import Adafruit_PCA9685
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-
 #Required for setting up DS4
 pygame.init()
 os.environ["SDL_VIDEORIVER"] = "x11"
@@ -24,9 +23,6 @@ buttons = {'x':0, 'o':0, 't':0, 's':0,
            'share':0, 'options':0,
            'axis1':0., 'axis2':0., 'axis3':0., 'axis4':0.}
 axiss=[0.,0.,0.,0.,0.,0.]
-
-
-
 
 '''
 
@@ -47,20 +43,23 @@ Rabbit Robot
         
 '''
 
-#Defining pins for motors
-m1p1 = 3
-m1p2 = 1
+#Defining pins for motors on PCA1(Motor)
+m1p1 = 1
+m1p2 = 3
 m1en = 2
 
-m2p1 = 7
-m2p2 = 5
+m2p1 = 5
+m2p2 = 7
 m2en = 6
 
-m3p1 = 11
-m3p2 = 9
+m3p1 = 9
+m3p2 = 11
 m3en = 10
 
-#Initializing 2 x PCA9685 with I2C address and PWM frequency
+#Defining pins for relay on PCA2
+relayin = 12
+
+#Initializing 2 x PCA9685 with I2C address0 and PWM frequency
 SET_FREQ = 100
 Motor = Adafruit_PCA9685.PCA9685(0x40)
 Motor.set_pwm_freq(SET_FREQ)
@@ -91,7 +90,9 @@ def getJS(name=''):
         return buttons[name]
     
 
-#Main program starts
+#Main program starts, giving a high signal
+PCA2.set_pwm(relayin, 0, 4095)
+flag=0
 while True:
 
     pygame.init()
@@ -99,55 +100,122 @@ while True:
     if movement == 'Joystick':
         jsVal = getJS()
     
-        if jsVal['axis1'] or jsVal['axis2']:
+        if jsVal['axis1'] or jsVal['axis2'] or jsVal['L2'] or jsVal['R2'] or jsVal['L1'] or jsVal['R1']:
             
-            Vx=float(jsVal['axis1'])    #Vx is the x component
-            Vy=-float(jsVal['axis2'])   #Vy is the y component
+            if jsVal['axis1'] or jsVal['axis2']:
+                
+                #These values are the x, y movement of the left joystick
+                Vx=float(jsVal['axis1'])    #Vx is the x component
+                Vy=-float(jsVal['axis2'])   #Vy is the y component
 
-            #Theses values are derived using inverse kinematics
-            m1speed=int(600*((-0.5*Vx)+(0.5*Vy)))
-            m2speed=int(600*((-0.5*Vx)+(-0.5*Vy)))
-            m3speed=int(600*((1*Vx)+(0*Vy)))
+                #Theses values are derived using inverse kinematics
+                m1speed=int(700*((-0.5*Vx)+(0.5*Vy)))
+                m2speed=int(700*((-0.5*Vx)+(-0.5*Vy)))
+                m3speed=int(700*((1*Vx)+(0*Vy)))
 
-            #Checking the individual motor values
-            print(m1speed,m2speed,m3speed)
+                #Checking the individual motor values
+                print(m1speed,m2speed,m3speed)
+                
+                #If the calculated speed is negative then the direction of the motor is reversed
+                if m1speed>=0:
+                    Motor.set_pwm(m1p1, 0, 4095) #4095 is highest PWM 
+                    Motor.set_pwm(m1p2, 0, 0) #0 is the lowest PWM
+                    Motor.set_pwm(m1en, 0, m1speed)
+                else:
+                    Motor.set_pwm(m1p1, 0, 0)
+                    Motor.set_pwm(m1p2, 0, 4095) 
+                    Motor.set_pwm(m1en, 0, abs(m1speed))
+                    
+                if m2speed>=0:
+                    Motor.set_pwm(m2p1, 0, 4095) 
+                    Motor.set_pwm(m2p2, 0, 0) 
+                    Motor.set_pwm(m2en, 0, m2speed)
+                else:
+                    Motor.set_pwm(m2p1, 0, 0) 
+                    Motor.set_pwm(m2p2, 0, 4095)
+                    Motor.set_pwm(m2en, 0, abs(m2speed))
+                    
+                if m3speed>=0:
+                    Motor.set_pwm(m3p1, 0, 4095) 
+                    Motor.set_pwm(m3p2, 0, 0)
+                    Motor.set_pwm(m3en, 0, m3speed)
+                else:
+                    Motor.set_pwm(m3p1, 0, 0) 
+                    Motor.set_pwm(m3p2, 0, 4095)
+                    Motor.set_pwm(m3en, 0, abs(m3speed))
+
+                    #Checking the joystick values from DS4     
+                    print("xaxis",jsVal['axis1'])
+                    print("yaxis",jsVal['axis2'])
+                    print("Bot move")
             
-            #If the calculated speed is negative then the direction of the motor is reversed
-            if m1speed>=0:
-                Motor.set_pwm(m1p1, 0, 4095) #4095 is highest PWM 
-                Motor.set_pwm(m1p2, 0, 0) #0 is the lowest PWM
-                Motor.set_pwm(m1en, 0, m1speed)
-            else:
-                Motor.set_pwm(m1p1, 0, 0)
-                Motor.set_pwm(m1p2, 0, 4095) 
-                Motor.set_pwm(m1en, 0, abs(m1speed))
+            elif jsVal['L2']:
+
+                #L2 rotates the bot anti-clockwise
+                Motor.set_pwm(m1p1, 0, 4095) 
+                Motor.set_pwm(m1p2, 0, 0)
+                Motor.set_pwm(m1en, 0, 350)
                 
-            if m2speed>=0:
-                Motor.set_pwm(m2p1, 0, 4095) 
-                Motor.set_pwm(m2p2, 0, 0) 
-                Motor.set_pwm(m2en, 0, m2speed)
-            else:
-                Motor.set_pwm(m2p1, 0, 0) 
-                Motor.set_pwm(m2p2, 0, 4095)
-                Motor.set_pwm(m2en, 0, abs(m2speed))
+                Motor.set_pwm(m2p1, 0, 4095)  
+                Motor.set_pwm(m2p2, 0, 0)
+                Motor.set_pwm(m2en, 0, 350)
                 
-            if m3speed>=0:
                 Motor.set_pwm(m3p1, 0, 4095) 
                 Motor.set_pwm(m3p2, 0, 0)
-                Motor.set_pwm(m3en, 0, m3speed)
-            else:
+                Motor.set_pwm(m3en, 0, 350)
+                
+                print("Bot Rotate Anti-Clockwise")
+            
+            elif jsVal['R2']:
+                
+                #R2 rotates the bot clockwise
+                Motor.set_pwm(m1p1, 0, 0) 
+                Motor.set_pwm(m1p2, 0, 4095)
+                Motor.set_pwm(m1en, 0, 350)
+                
+                Motor.set_pwm(m2p1, 0, 0)  
+                Motor.set_pwm(m2p2, 0, 4095)
+                Motor.set_pwm(m2en, 0, 350)
+                
                 Motor.set_pwm(m3p1, 0, 0) 
                 Motor.set_pwm(m3p2, 0, 4095)
-                Motor.set_pwm(m3en, 0, abs(m3speed))
-
-            #Checking the joystick values from DS4     
-            print("xaxis",jsVal['axis1'])
-            print("yaxis",jsVal['axis2'])
-            print("Bot move")
-        
+                Motor.set_pwm(m3en, 0, 350) 
+                
+                print("Bot Rotate Clockwise")
+            
+            elif jsVal['L1']:
+                
+                #L1 drifts the bot towards left side
+                Motor.set_pwm(m1p1, 0, 4095) 
+                Motor.set_pwm(m1p2, 0, 0)
+                Motor.set_pwm(m1en, 0, 350)
+                
+                Motor.set_pwm(m2p1, 0, 0)  
+                Motor.set_pwm(m2p2, 0, 4095)
+                Motor.set_pwm(m2en, 0, 350)
+                
+                Motor.set_pwm(m3p1, 0, 0) 
+                Motor.set_pwm(m3p2, 0, 4095)
+                Motor.set_pwm(m3en, 0, 350)
+            
+            elif jsVal['R1']:
+                
+                #R1 drifts the bot towards right side
+                Motor.set_pwm(m1p1, 0, 4095)
+                Motor.set_pwm(m1p2, 0, 0)
+                Motor.set_pwm(m1en, 0, 350)
+                
+                Motor.set_pwm(m2p1, 0, 0)  
+                Motor.set_pwm(m2p2, 0, 4095)
+                Motor.set_pwm(m2en, 0, 350)
+                
+                Motor.set_pwm(m3p1, 0, 4095) 
+                Motor.set_pwm(m3p2, 0, 0)
+                Motor.set_pwm(m3en, 0, 350)                 
+            
         else:
 
-            #If the joystick is not moved then both the pins of the motor will be given a high signal
+            #If the joystick is not moved or L1,R1,L2,R2 are not pressed then both the pins of the motor will be given a high signal
             Motor.set_pwm(m1p1, 0, 4095) 
             Motor.set_pwm(m1p2, 0, 4095)
             Motor.set_pwm(m1en, 0, 0)
@@ -158,9 +226,35 @@ while True:
             
             Motor.set_pwm(m3p1, 0, 4095) 
             Motor.set_pwm(m3p2, 0, 4095)
-            Motor.set_pwm(m3en, 0, 0)
+            Motor.set_pwm(m3en, 0, 0) 
 
             #Checking if this block is executed or not
-            print("Bot stop")
+            print("Bot stop")    
+        
+        if jsVal['t']:
+
+            #This loop is used to take the push of Triangle button as a single command regardless of the duration it is pressed for
+            while True:
+                temp = getJS()
+                if temp['t']:
+                    continue
+                else:
+                    break 
+
+            #Triangle is used to send a low signal to the relay, and flag decides the duration for how long the relay will get a high signal
+            PCA2.set_pwm(relayin, 0, 0)
+            flag=15
+            print("Relay on")
+        
+        if flag==1:
+            
+            #If flag reaches 1, then the relay is given a high signal
+            PCA2.set_pwm(relayin, 0, 4095)
+            flag=0
+            print("Relay off")
+        
+        if flag>0:
+            
+            flag-=1
                         
     time.sleep(0.001)
