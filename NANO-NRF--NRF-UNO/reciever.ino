de2d89dd8de4a04588a777d98347d0735f300ec9
saltylemonz <74402307+SaltyLemonz@users.noreply.github.com>
2023-04-08 18:33:57 +0530

    UNO

--- NANO-NRF--NRF-UNO/reciever.ino
+++ NANO-NRF--NRF-UNO/reciever.ino
@@ -1,9 +1,10 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include "DigitalIO.h"
 
-
-int i;
+// analog values[0-255] ----> (x_L, y_L, x_R, y_R), (SELECT,L3,R3,START,UP,RIGHT,DOWN,LEFT,L2,R2,L1,R1,TRIANGLE,CIRCLE,CROSS,SQUARE) <---- boolean values[0-1]
+uint8_t PS_arr[20];
 
 RF24 radio(8, 10); // CE, CSN
 
@@ -12,6 +13,9 @@ const byte address[6] = "00001";
 void setup() {
   Serial.begin(9600);
   radio.begin();
+  if (radio.isChipConnected()) {
+    Serial.println("NRF24L01+ CONNECTED TO SPI BUS.");
+  }
   radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MAX);
   radio.startListening();
@@ -19,8 +23,14 @@ void setup() {
 
 void loop() {
   if (radio.available()) {
-    radio.read(&i, sizeof(i));
-    Serial.println(i);
+    radio.read(&PS_arr, sizeof(PS_arr));
   }
+    for (int j=0; j<=19; j++) {    
+      Serial.print(PS_arr[j]); d
+      Serial.print(", ");
+    }
+  Serial.println();
   delay(10);
 }
+
+//reciever
