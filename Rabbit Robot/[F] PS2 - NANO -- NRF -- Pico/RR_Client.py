9a70dd38f1955807c8012e574a8189679ef5cac6
Pranav Malakar <92641237+pranav-malakar@users.noreply.github.com>
2023-05-07 15:58:08 +0530

    Speed reduced

--- Rabbit Robot/[F] PS2 - NANO -- NRF -- Pico/RR_Client.py
+++ Rabbit Robot/[F] PS2 - NANO -- NRF -- Pico/RR_Client.py
@@ -53,7 +53,7 @@ m3p2 = Pin(11,Pin.OUT)
 m3en = PWM(Pin(10))
 
 #Function for reading data from Pico2
-no_of_channels=6
+no_of_channels=4
 command = [0]*no_of_channels
 def readval():
     
@@ -74,9 +74,9 @@ def readval():
 def botmove(Vx,Vy): #Vx is the x component and Vy is the y component
     
     #Theses values are derived using inverse kinematics
-    m1speed=int(640*((-0.5*Vx)+(0.5*Vy)))
-    m2speed=int(640*((-0.5*Vx)+(-0.5*Vy)))
-    m3speed=int(640*((1*Vx)+(0*Vy)))
+    m1speed=int(250*((-0.5*Vx)+(0.5*Vy)))
+    m2speed=int(250*((-0.5*Vx)+(-0.5*Vy)))
+    m3speed=int(250*((1*Vx)+(0*Vy)))
     
     #Checking the individual motor values
     print("Bot move -",m1speed,m2speed,m3speed)
@@ -104,15 +104,15 @@ def botrotate(dir):
     
     m1p1.value(not dir)
     m1p2.value(dir)
-    m1en.duty_u16(16000)
+    m1en.duty_u16(8000)
     
     m2p1.value(not dir)
     m2p2.value(dir)
-    m2en.duty_u16(16000)
+    m2en.duty_u16(8000)
     
     m3p1.value(not dir)
     m3p2.value(dir)
-    m3en.duty_u16(16000)
+    m3en.duty_u16(8000)
 
 #This function is used for stopping the bot
 def botstop():
@@ -135,12 +135,12 @@ def botstop():
 while True:
 
     readval()
-    if command[0] or command[1]: #command[0] and command[1] is x and y motion of joystick
+    if command[0] or command[1]: #Right Joystick is used for x and y motion of the bot
         botmove(-command[0],command[1])
-    elif command[4]: #command[4] is used for rotating the bot clockwise
+    elif command[2]: #L1 is used for rotating the bot clockwise
         botrotate(dir=0)
-    elif command[5]: #command[4] is used for rotating the bot anti-clockwise
+    elif command[3]: #R1 is used for rotating the bot anti-clockwise
         botrotate(dir=1)
     else:  
         botstop()
-    sleep(0.005)
+    sleep(0.01)
